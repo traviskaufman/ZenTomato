cb02e93e0254048d33a37ae8144a5820ecba114e
Travis Kaufman <travis.kaufman@gmail.com>
2019-09-21 08:08:47 -0400

    Mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,166 @@
-import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { ThemeProvider } from 'emotion-theming';
 import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Play } from './Play.svg';
+import { ReactComponent as Pause } from './Pause.svg';
+import { ReactComponent as Stop } from './Stop.svg';
+
+const theme = {
+  primary: '#f06b50',
+  secondary: '#fff',
+  textOnPrimary: '#fff',
+  textOnSecondary: '#121212',
+}
+
+const cssHelpers = {
+  btnReset: css`
+    background: none;
+    border: none;
+    appearance: none;
+  `,
+};
+
+const styles = {
+  logo: css`
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    opacity: 0.7;
+    transition: opacity 125ms ease;
+
+    &:hover {
+      opacity: 1;
+    }
+  `,
+  time: css`
+    font-size: 12rem;
+    @media (max-width: 720px) {
+      font-size: 6rem;
+    }
+    font-weight: 300;
+    line-height: 1;
+    letter-spacing: -.015rem;
+    margin: 20px auto;
+  `,
+  appContainer: theme => css`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: ${theme.textOnPrimary};
+  `,
+  appUI: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `,
+  controls: css`
+    display: flex;
+    align-items: center;
+    z-index: 1;
+  `,
+  control: css`
+    ${cssHelpers.btnReset}
+    transition: transform 125ms ease;
+    opacity: 1;
+    cursor: pointer;
+    margin-right: 24px;
+    &:last-child {
+      margin-right: 0;
+    }
+    &:hover {
+      transform: scale(1.08);
+    }
+  `,
+  segmentBar: theme => css`
+    box-sizing: border-box;
+    border-radius: 8px;
+    width: 100%;
+    height: 48px;
+    border: 1px solid ${theme.secondary};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 1;
+  `,
+  segmentControl: theme => css`
+    ${cssHelpers.btnReset}
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0);
+    color: ${theme.textOnPrimary};
+    border-right: 1px solid ${theme.textOnPrimary};
+    display: block;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    font-size: 1rem;
+    line-height: 1;
+    letter-spacing: 0.5;
+    transition: color 125ms ease, background-color 125ms ease;
+
+    &:first-child {
+      border-top-left-radius: inherit;
+      border-bottom-left-radius: inherit;
+    }
+
+    &:last-child {
+      border-right: none;
+      border-bottom-right-radius: inherit;
+      border-top-right-radius: inherit;
+    }
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.4);
+    }
+  `,
+  footer: theme => css`
+    font-size: 0.75rem;
+    opacity: 0.7;
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 20px;
+
+    a {
+      color: ${theme.textOnPrimary};
+      text-decoration: none;
+
+      &:hover,
+      &:active,
+      &:focus {
+        text-decoration: underline;
+      }
+    }
+  `
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div css={styles.appContainer}>
+        {/* TODO: Hidden text in link */}
+        <a css={styles.logo} href="#"><img src={logo} alt="ZenTomato" width="96" height="96" /></a>
+        <main css={styles.appUI}>
+          <section css={styles.segmentBar}>
+            <button css={styles.segmentControl} title="cmd+p" onClick={() => console.debug('pomodoro')}>Pomodoro</button>
+            <button css={styles.segmentControl} title="cmd+b" onClick={() => console.debug('shortBreak')}>Short break</button>
+            <button css={styles.segmentControl} title="cmd+shift+b" onClick={() => console.debug('longBreak')}>Long break</button>
+          </section>
+          <time css={styles.time} dateTime="24:05">24:05</time>
+          <div css={styles.controls}>
+            {/* TODO: Button role? Button itself? */}
+            <button css={styles.control} title="spacebar"><Play /></button>
+            <button css={styles.control} title="cmd+."><Stop /></button>
+          </div>
+        </main>
+        <footer css={styles.footer}><p>A project by <a href="https://traviskaufman.io" target="_blank">Travis Kaufman</a></p></footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
